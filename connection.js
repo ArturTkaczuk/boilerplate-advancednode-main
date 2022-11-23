be9b9d40f1f7a2889404840a950fa8628c711952
Artur Tkaczuk <tkaczukartur@gmail.com>
2022-11-23 19:10:57 +0100

    i'm getting frustrated

--- connection.js
+++ connection.js
@@ -3,11 +3,14 @@ require("dotenv").config();
 const { MongoClient } = require("mongodb");
 
 async function main(callback) {
-  const URI = process.env.MONGO_URI; // Declare MONGO_URI in your .env file
-  const client = new MongoClient(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  //   const URI = process.env.MONGO_URI; // Declare MONGO_URI in your .env file
+  const client = new MongoClient(
+    "mongodb+srv://Admin:cH@M4KXvPHMWqa5@cluster0.38rnvbr.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
 
   try {
     // Connect to the MongoDB cluster
