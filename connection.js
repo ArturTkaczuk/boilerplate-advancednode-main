ce94e1f4263ede8f747ce9274f1be7f66b07e05f
Artur Tkaczuk <tkaczukartur@gmail.com>
2022-11-23 19:13:57 +0100

    stuff

--- connection.js
+++ connection.js
@@ -3,14 +3,11 @@ require("dotenv").config();
 const { MongoClient } = require("mongodb");
 
 async function main(callback) {
-  //   const URI = process.env.MONGO_URI; // Declare MONGO_URI in your .env file
-  const client = new MongoClient(
-    "mongodb+srv://Admin:cH@M4KXvPHMWqa5@cluster0.38rnvbr.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  );
+  const URI = process.env.MONGO_URI; // Declare MONGO_URI in your .env file
+  const client = new MongoClient(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
   try {
     // Connect to the MongoDB cluster
@@ -18,7 +15,6 @@ async function main(callback) {
 
     // Make the appropriate DB calls
     await callback(client);
-    console.log("Successfully connected to the Database");
   } catch (e) {
     // Catch any errors
     console.error(e);
